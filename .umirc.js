df2acd36b25772e276115d256262c1e9e62ecbc8
wuqingshan <wuqingshan@xylink.com>
2020-12-07 15:26:42 +0800

    fix: make pages visit from base

--- .umirc.js
+++ .umirc.js
@@ -4,19 +4,19 @@ export default {
     type: 'none',
     exclude: [],
   },
+  base: "/xy-hooks",
   outputPath: "/docs",
   mode: 'site',
   title: 'xy-hooks',
-  favicon: '/favicon.ico',
-  logo: '/logo.png',
+  favicon: '/xy-hooks/favicon.ico',
+  logo: '/xy-hooks/logo.png',
   dynamicImport: {},
   manifest: {},
-  links: [{ rel: 'manifest', href: '/asset-manifest.json' }],
   hash: true,
   resolve: {
     includes: ['guide', 'packages/hooks/src'],
   },
-  links: [{ rel: 'stylesheet', href: '/style.css' }],
+  links: [{ rel: 'stylesheet', href: '/xy-hooks/style.css' }],
   navs: {
     'zh-CN': [
       // 加载默认的 nav 选项
